ac912230cdf28545c1a15718e7d7ce888f0c70cb
moonveil <tz59pk@gmail.com>
2024-03-08 14:35:53 +0800

    update

--- lazyvim/lua/plugins/editor.lua
+++ lazyvim/lua/plugins/editor.lua
@@ -128,15 +128,6 @@ return {
     },
   },
   {
-    "hedyhli/outline.nvim",
-    lazy = true,
-    cmd = { "Outline", "OutlineOpen" },
-    -- keys = { -- Example mapping to toggle outline
-    --   { "<leader>uo", "<cmd>Outline<CR>", desc = "Toggle outline" },
-    -- },
-    opts = {},
-  },
-  {
     "keaising/im-select.nvim",
     event = "VeryLazy",
     config = function()
